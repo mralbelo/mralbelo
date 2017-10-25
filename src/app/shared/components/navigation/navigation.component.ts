4763ccdf4055171aa4abebafad9a5b3041bc1b65
Juan Albelo <albelo.jluis@live.com>
2017-10-25 10:29:04 -0500

    changes

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -19,6 +19,7 @@ export class NavigationComponent implements OnInit {
         } else if (distanceY < shrinkOn) {
             header.classList.remove('bg-dark');
         }
+        
     });
   }
 
